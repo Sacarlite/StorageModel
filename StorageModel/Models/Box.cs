c46aaf3307306f3e510e9b85a99feb39d7d89653
SacarLite <vladen.bokovoi@mail.ru>
2025-06-06 16:15:44 +0300

    Реализованны базовые модели элементов хранения

---
+++ StorageModel/Models/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageModel.Models
+{
+    public class Box : StorageItem
+    {
+        public DateTime? ProductionDate { get; set; }
+        public DateTime? ExpirationDateOverride { get; set; }
+
+        protected override DateTime CalculateExpirationDate()
+        {
+            if (ExpirationDateOverride.HasValue)
+            {
+                return ExpirationDateOverride.Value.Date;
+            }
+            if (ProductionDate.HasValue)
+            {
+                return ProductionDate.Value.Date.AddDays(100);
+            }
+            throw new InvalidOperationException("Не указана дата производства или срок годности.");
+        }
+    }
+}
---
+++ StorageModel/Models/Pallet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageModel.Models
+{
+    public class Pallet : StorageItem
+    {
+        private List<Box> _boxes = new List<Box>();
+
+        public void AddBox(Box box)
+        {
+            if (box.Width > this.Width || box.Depth > this.Depth)
+            {
+                throw new InvalidOperationException("Размеры коробки превышают размеры паллеты.");
+            }
+
+            _boxes.Add(box);
+        }
+
+        protected override DateTime CalculateExpirationDate()
+        {
+            if (!_boxes.Any())
+                throw new InvalidOperationException("Паллета не содержит коробок.");
+
+            return _boxes.Min(b => b.ExpirationDate).Date;
+        }
+
+        public override double Weight => _boxes.Sum(b => b.Weight) + 30;
+
+        public override double Volume => base.Volume + _boxes.Sum(b => b.Volume);
+    }
+}
